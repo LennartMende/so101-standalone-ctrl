39ddcb1a8ae704bbc30ae37b4cf3f290c313522e
LennartMende <166758749+LennartMende@users.noreply.github.com>
2026-08-28 12:20:33 +0200

    Delete web-project directory

--- web-project/mqtt/constants.py
+++
@@ -1,6 +0,0 @@
-# constants.py
-BROKER = "192.168.7.27"
-# BROKER = "localhost" # for local DEBUGGING
-PORT = 8883 # 1883 for standard MQTT, 8883 for secure MQTT
-USERNAME = "username"
-PASSWORD = "password"
